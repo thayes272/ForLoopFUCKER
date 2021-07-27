3c01c75ea257dcd667a991b63140cdcf99f19d3e
Dax272 <49327226+Dax272@users.noreply.github.com>
2021-07-27 00:01:08 -0700

    FUCK YoU GiThUb
    jk love you <3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FORLOOPghjkasrowieru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++) //loops 10 times
+            {
+                Console.WriteLine(rand.Next(10));
+            }
+            Console.WriteLine("Damn thats a lot of numbers, thanks for tuning in to this shitty c# program");
+        }
+    }
+}
